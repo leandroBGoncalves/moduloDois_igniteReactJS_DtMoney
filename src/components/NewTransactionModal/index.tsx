c4f915ce568f98bf538b10b758dc90b3f6bb9e34
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-06-05 14:49:45 -0400

    Finalizando a estilizaçao do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
-import { Container } from './styles';
+import CloseImg from '../../assets/close.svg';
+import IncomeImg from '../../assets/income.svg';
+import OutcomeImg from '../../assets/outcome.svg';
+import { Container, RadiuButton, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -7,6 +11,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    //Abaixo estado utilizado para guardar a categoria selecionada, entrada ou saida
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal 
         isOpen={isOpen}
@@ -14,6 +21,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         overlayClassName="react-modal-overlay"
         className="react-modal-content"
         >
+
+            <button 
+            type="button" 
+            onClick={onRequestClose}
+            className="react-modal-close"
+            >
+                <img src={CloseImg} alt="Fechar Modal"/>
+            </button>
 
             <Container>
             <h2>Cadastrar Transação</h2>
@@ -27,6 +42,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 type="number"
                 placeholder="Valor"
             />
+
+            <TransactionTypeContainer>
+                <RadiuButton 
+                    type="button"
+                    onClick={() => { setType('deposit'); }}
+                    isActive={type == 'deposit'} //retorna true se for selecionado e false se não for, isso só funciona em components
+                    activeColor="greem"
+                >
+                    <img src={IncomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </RadiuButton>
+
+                <RadiuButton 
+                    type="button"
+                    onClick={() => { setType('withdraw'); }}
+                    isActive={type == 'withdraw'}
+                    activeColor="red"
+                >
+                    <img src={OutcomeImg} alt="Saida"/>
+                    <span>Saida</span>
+                </RadiuButton>
+            </TransactionTypeContainer>
 
             <input 
                 placeholder="Categoria"
