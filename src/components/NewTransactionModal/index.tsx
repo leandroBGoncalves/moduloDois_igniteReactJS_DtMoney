32515107a4c77dae43f6a2edd00964e9e7802ab7
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-06-05 22:37:42 -0400

    Configurando api para receber dados do Form e armazenalos no Banco de dados Fake

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import CloseImg from '../../assets/close.svg';
 import IncomeImg from '../../assets/income.svg';
 import OutcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, RadiuButton, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
@@ -12,7 +13,25 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     //Abaixo estado utilizado para guardar a categoria selecionada, entrada ou saida
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
+
+    //Abaixo função para pegar as informações passadas pelo usuario
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+        const data = {
+            title,
+            value,
+            category,
+            type,
+        };
+
+        api.post('/transactions', data)
+        
+    }
 
     return (
         <Modal 
@@ -30,17 +49,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <img src={CloseImg} alt="Fechar Modal"/>
             </button>
 
-            <Container>
+            <Container onClick={handleCreateNewTransaction}>
             <h2>Cadastrar Transação</h2>
 
 
             <input 
                 placeholder="Titulo"
+                value={title}
+                //função abaixo executa sempre que um novo valor é digitado e captura esse Valor
+                onChange={event => setTitle(event.target.value)}
             />
 
             <input 
                 type="number"
                 placeholder="Valor"
+                value={value}
+                //função abaixo executa sempre que um novo valor é digitado e captura esse Valor
+                onChange={event => setValue(Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
@@ -67,6 +92,9 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
             <input 
                 placeholder="Categoria"
+                value={category}
+                //função abaixo executa sempre que um novo valor é digitado e captura esse Valor
+                onChange={event => setCategory(event.target.value)}
             />
 
             <button type="submit">
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 
 createServer({
+
+  //abaixo primeira "tabela do banco de dados do mirage"
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
-
+// abaixo pega do Banco de dados e manda de volta
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          creatAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+//abaixo rota para que o mirage consiga receber os dados do formulario e enviar para o banco de dados
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
