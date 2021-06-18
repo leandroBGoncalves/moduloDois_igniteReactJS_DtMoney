1ac8b33735c8dc3c910997045e1feb7efbdf9ea5
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-06-18 13:31:42 -0400

    finalizando a criaçao do contexto

---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,34 @@
+import {createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from './services/api';
+
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+
+}
+
+interface TransactionsTableProviderProps {
+    children: ReactNode;
+}
+
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+
+export function TransactionsTableProvider({children}: TransactionsTableProviderProps) {
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    useEffect(() => {
+        api.get('/transactions')
+        .then(response => setTransactions(response.data.transactions))
+    }, []);
+
+    return (
+        <TransactionsContext.Provider value={transactions}>
+            {children}
+        </TransactionsContext.Provider>
+    );
+}
